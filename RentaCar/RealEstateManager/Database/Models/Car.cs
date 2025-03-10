9e84bafcd2a4c8ea8c7445c1c5f81bec54b286d3
HristiyanIliev06 <hristiyan2006@gmail.com>
2025-03-10 19:11:19 +0200

    Finishing touches to Car model

--- RentaCar/RealEstateManager/Database/Models/Car.cs
+++ RentaCar/RealEstateManager/Database/Models/Car.cs
@@ -1,16 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using ServiceStack.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RentaCar.RealEstateManager.Database.Models
 {
+    [Table("Car", Schema = "blg")]
     public class Car
     {
-        [Key]
-        public int Id { get; set; }
-        public string Brand { get; set; }
-        public string Model { get; set; }
+        [PrimaryKey]
+        public int Pk { get; set; }
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
         public int Year { get; set; }
         public decimal PricePerDay { get; set; }
-        public bool IsAvailable { get; set; } = true;
+        public int PassangerSeats { get; set; } 
+        public string? Description { get; set; }
         public  string? Picture { get; set; }
         public ICollection<BookingViewModel> Bookings { get; set; } = new List<BookingViewModel>();
     }
--- RentaCar/RealEstateManager/WebApp/Models/CarViewModel.cs
+++ RentaCar/RealEstateManager/WebApp/Models/CarViewModel.cs
@@ -4,14 +4,14 @@ namespace RentaCar.RealEstateManager.WebApp.Models
 {
     public class CarViewModel
     {
-      
-        public int Id { get; set; }
-        public string Brand { get; set; }
-        public string Model { get; set; }
+
+        public int Pk { get; set; }
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
         public int Year { get; set; }
         public decimal PricePerDay { get; set; }
-        public bool IsAvailable { get; set; } = true;
+        public int PassangerSeats { get; set; }
+        public string? Description { get; set; }
         public string? Picture { get; set; }
-        public ICollection<BookingViewModel> Bookings { get; set; } = new List<BookingViewModel>();
     }
 }
