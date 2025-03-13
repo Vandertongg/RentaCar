70a05c7667f12f64ae4593a9c355463fd88ee44e
Ясмин Тонг <yasmin.tong1707@gmail.com>
2025-03-13 19:32:09 +0200

    CarSearchViewModels.cs

---
+++ RentaCar/RealEstateManager/WebApp/Models/CarSearchViewModel.cs
@@ -0,0 +1,22 @@
+﻿using RentaCar.RealEstateManager.Database.Data.Entities;
+
+namespace RentaCar.RealEstateManager.WebApp.Models
+{
+    public class CarSearchViewModel
+    {
+        public string? SearchTerm { get; set; }
+        public string? Brand { get; set; }
+        public int? Year { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public int? MinSeats { get; set; }
+        public bool? IsAvailable { get; set; }
+        public string? SortBy { get; set; }
+        public List<Car> Cars { get; set; } = new List<Car>();
+
+        
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; } = 5; 
+    }
+}
