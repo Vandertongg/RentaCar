8365b0bfcecead75ae9ff40d37dc1337634f026b
Ясмин Тонг <yasmin.tong1707@gmail.com>
2025-03-30 22:54:10 +0300

    Creating ManageRoles.cshtml  and Admin can change some roles

--- RentaCar/RealEstateManager/API/Controllers/AdminController.cs
+++ RentaCar/RealEstateManager/API/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RentaCar.RealEstateManager.Database.Data.Entities;
+using RentaCar.RealEstateManager.WebApp.Models;
 using System.Threading.Tasks;
 
 namespace RentaCar.RealEstateManager.API.Controllers
@@ -11,10 +12,12 @@ namespace RentaCar.RealEstateManager.API.Controllers
     public class AdminController : Controller
     {
         private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AdminController(UserManager<User> userManager)
+        public AdminController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         // GET: User
@@ -161,6 +164,62 @@ namespace RentaCar.RealEstateManager.API.Controllers
             }
 
             return View(user);
+        }
+        public async Task<IActionResult> ManageRoles(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound($"User with ID = {userId} cannot be found.");
+            }
+
+            var roles = await _roleManager.Roles.ToListAsync();
+            var model = new List<UserViewModel>();
+
+            foreach (var role in roles)
+            {
+                model.Add(new UserViewModel
+                {
+                    RoleName = role.Name,
+                    IsSelected = await _userManager.IsInRoleAsync(user, role.Name)
+                });
+            }
+
+            ViewBag.UserId = userId;
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ManageRoles(string userId, List<UserViewModel> model)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound($"User with ID = {userId} cannot be found.");
+            }
+
+            var currentRoles = await _userManager.GetRolesAsync(user);
+
+            // Премахване на всички текущи роли, които не са избрани
+            foreach (var role in currentRoles)
+            {
+                if (!model.Any(x => x.RoleName == role && x.IsSelected))
+                {
+                    await _userManager.RemoveFromRoleAsync(user, role);
+                }
+            }
+
+            // Добавяне на новоизбраните роли
+            foreach (var role in model.Where(x => x.IsSelected).Select(x => x.RoleName))
+            {
+                if (!currentRoles.Contains(role))
+                {
+                    await _userManager.AddToRoleAsync(user, role);
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
