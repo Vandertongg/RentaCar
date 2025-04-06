982e77e752bfed49776c5da0275d87b823c237dd
HristiyanIliev06 <hristiyan2006@gmail.com>
2025-04-06 15:42:09 +0300

    Adding IBookingService

---
+++ RentaCar/RealEstateManager/API/Services/IBookingService.cs
@@ -0,0 +1,12 @@
+﻿namespace RentaCar.RealEstateManager.API.Services
+{
+    public interface IBookingService
+    {
+        Task<bool> ApproveBookingAsync(int bookingId, string userRole);
+        Task<bool> RejectBookingAsync(int bookingId, string userRole, string? rejectionReason = null);
+
+        // Добавяне на метода за валидация на дати
+        (bool isValid, string message) ValidateDates(DateTime startDate, DateTime endDate);
+    }
+
+}
