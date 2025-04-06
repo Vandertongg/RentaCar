303d3cabbbe61145bb581d6a2b584b323aae9be4
Ерхан Неби <erhanemin594@gmail.com>
2025-04-06 16:22:38 +0300

    Sort user

--- RentaCar/RealEstateManager/API/Controllers/AdminController.cs
+++ RentaCar/RealEstateManager/API/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RentaCar.RealEstateManager.Database.Data.Entities;
+using RentaCar.RealEstateManager.WebApp.Models;
 using System.Threading.Tasks;
 
 namespace RentaCar.RealEstateManager.API.Controllers
@@ -11,18 +12,39 @@ namespace RentaCar.RealEstateManager.API.Controllers
     public class AdminController : Controller
     {
         private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AdminController(UserManager<User> userManager)
+        public AdminController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
+
         // GET: User
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
-            var users = await _userManager.Users.ToListAsync();
-            return View(users);
+            // Запазване на текущата стойност на филтъра за използване в изгледа
+            ViewData["CurrentFilter"] = searchString;
+
+            // Извличане на всички потребители
+            var users = _userManager.Users.AsQueryable();
+
+            // Логика за филтриране
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                users = users.Where(u => u.FirstName.Contains(searchString) ||
+                                         u.LastName.Contains(searchString) ||
+                                         u.Email.Contains(searchString) ||
+                                         u.UserName.Contains(searchString));
+            }
+
+            // Конвертиране на резултата в списък
+            var userList = await users.ToListAsync();
+
+            return View(userList);
         }
+
 
 
         // GET: User/Details/5
@@ -92,44 +114,48 @@ namespace RentaCar.RealEstateManager.API.Controllers
         // POST: User/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(User user)
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, User updatedUser, IFormFile ProfilePicture)
         {
-            if (!ModelState.IsValid)
-            {
-                return View(user);
-            }
-
-            var existingUser = await _userManager.FindByIdAsync(user.Id);
-            if (existingUser == null)
+            // Намиране на потребителя по ID
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
             {
                 return NotFound();
             }
 
-            // Актуализирайте полетата на потребителя
-            existingUser.FirstName = user.FirstName;
-            existingUser.LastName = user.LastName;
-            existingUser.IdentificationNumber = user.IdentificationNumber;
-            existingUser.Email = user.Email;
-
-            var result = await _userManager.UpdateAsync(existingUser);
-
-            if (result.Succeeded)
+            // Актуализиране на профилната снимка
+            if (ProfilePicture != null && ProfilePicture.Length > 0)
             {
-                return RedirectToAction(nameof(Index));
+                // Генериране на уникално име за файла
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(ProfilePicture.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
+
+                // Запазване на файла в "wwwroot/images"
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await ProfilePicture.CopyToAsync(stream);
+                }
+
+                // Запазване на пътя към файла в базата данни
+                user.ProfilePicture = "/images/" + fileName;
             }
 
-            foreach (var error in result.Errors)
+            // Актуализиране на другите полета
+            user.FirstName = updatedUser.FirstName;
+            user.LastName = updatedUser.LastName;
+            user.Age = updatedUser.Age;
+            user.IdentificationNumber = updatedUser.IdentificationNumber;
+
+            // Запазване на промените в базата данни
+            var result = await _userManager.UpdateAsync(user);
+            if (!result.Succeeded)
             {
-                ModelState.AddModelError(string.Empty, error.Description);
+                ModelState.AddModelError("", "Failed to update user.");
+                return View(updatedUser);
             }
 
-            return View(user);
-        }
-
-        // GET: User/Create
-        public IActionResult Create()
-        {
-            return View(new User());
+            return RedirectToAction("Index");
         }
 
         // POST: User/Create
@@ -162,5 +188,63 @@ namespace RentaCar.RealEstateManager.API.Controllers
 
             return View(user);
         }
+
+        public async Task<IActionResult> ManageRoles(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound($"User with ID = {userId} cannot be found.");
+            }
+
+            var roles = await _roleManager.Roles.ToListAsync();
+            var model = new List<UserViewModel>();
+
+            foreach (var role in roles)
+            {
+                model.Add(new UserViewModel
+                {
+                    RoleName = role.Name,
+                    IsSelected = await _userManager.IsInRoleAsync(user, role.Name)
+                });
+            }
+
+            ViewBag.UserId = userId;
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ManageRoles(string userId, List<UserViewModel> model)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound($"User with ID = {userId} cannot be found.");
+            }
+
+            var currentRoles = await _userManager.GetRolesAsync(user);
+
+            // Премахване на всички текущи роли, които не са избрани
+            foreach (var role in currentRoles)
+            {
+                if (!model.Any(x => x.RoleName == role && x.IsSelected))
+                {
+                    await _userManager.RemoveFromRoleAsync(user, role);
+                }
+            }
+
+            // Добавяне на новоизбраните роли
+            foreach (var role in model.Where(x => x.IsSelected).Select(x => x.RoleName))
+            {
+                if (!currentRoles.Contains(role))
+                {
+                    await _userManager.AddToRoleAsync(user, role);
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
